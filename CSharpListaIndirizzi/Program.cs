4221219b0d1c99141503b1b0876b866ade04b09e
thrudr <ariel.oliveiraesilva@gmail.com>
2023-04-26 16:35:59 +0200

    creation-list;

--- CSharpListaIndirizzi/Program.cs
+++ CSharpListaIndirizzi/Program.cs
@@ -1,2 +1,27 @@
 ﻿
 StreamReader file = File.OpenText("C:/Users/generation/Downloads/addresses.csv");
+List<AddressList> Adresses = new List<AddressList>();
+
+int readedString = 0;
+
+while (!file.EndOfStream)
+{
+    readedString++;
+    string line = file.ReadLine();
+
+    if (readedString == 1)
+    {
+        continue;
+    }
+
+    string[] dateInString = line.Split(',');
+    for (int i = 0; i < dateInString.Length; i++)
+    {
+        if (dateInString[i] == "")
+        {
+            dateInString[i] = "Undefined";
+        }
+    }
+
+
+}
