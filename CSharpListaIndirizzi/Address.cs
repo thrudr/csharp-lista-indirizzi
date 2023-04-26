2583cd7634c7ae3b3f1866ac94e82fc6d18776d9
thrudr <ariel.oliveiraesilva@gmail.com>
2023-04-26 15:30:40 +0200

    address-class; attributes-address-class

---
+++ CSharpListaIndirizzi/Address.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressClass
+{
+    public class Address
+    {
+        //ATTRIBUTES
+
+        private string name;
+        private string surname;
+        private string street;
+        private string city;
+        private string province;
+        private string zip;
+
+        //CONTRUCTORS
+        public string address(string name, string surname, string street, string city, string province, string zip);
+
+    }
